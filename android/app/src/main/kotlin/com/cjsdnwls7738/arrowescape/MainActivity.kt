04ec1f6998ba03386734043cb1e44e1a223cacf9
dnwls7738 <cjsdnwls7738@gmail.com>
2026-03-12 11:38:30 +0900

    Fix Android 15 Edge-To-Edge display warning

--- android/app/src/main/kotlin/com/cjsdnwls7738/arrowescape/MainActivity.kt
+++ android/app/src/main/kotlin/com/cjsdnwls7738/arrowescape/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.cjsdnwls7738.arrowescape
 
-import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Android 15 (SDK 35) 이상에서 앱이 전체 화면으로 올바르게 표시되도록 설정합니다.
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+    }
+}
