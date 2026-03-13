2eba523be8a6a5283f141f1376148178de44ee63
dnwls7738 <cjsdnwls7738@gmail.com>
2026-03-13 10:35:16 +0900

    Fix startup crash: revert MainActivity and use qualified styles for Android 15 opt-out

--- android/app/src/main/kotlin/com/cjsdnwls7738/arrowescape/MainActivity.kt
+++ android/app/src/main/kotlin/com/cjsdnwls7738/arrowescape/MainActivity.kt
@@ -1,13 +1,6 @@
 package com.cjsdnwls7738.arrowescape
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        // Android 15 (SDK 35) 이상에서 앱이 전체 화면으로 올바르게 표시되도록 설정합니다.
-        enableEdgeToEdge()
-        super.onCreate(savedInstanceState)
-    }
+class MainActivity: FlutterActivity() {
 }
